cf539e33ba6104485668b181161b3dd846cb033b
Tsingakis MP <marie-pier.1997@hotmail.com>
2020-03-06 17:37:05 -0500

    Evenement ok

--- category-evenements.php
+++ category-evenements.php
@@ -1,42 +1,77 @@
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * The template for displaying archive pages
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package underscores
  */
 
-$args3 = array(
-    "category_name" => "evenements",
-    "posts_per_page"=> -1,
-);
-
-$query3 = new WP_Query( $args3 );
-
 get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+<?php
+        echo '<h1 class="titreSections"> '.category_description( get_category_by_slug( 'evenements' ) ). '</h1>'   ;
+         
+         echo '<div class="evenements">';
+
+		 if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php
+			//	the_archive_title( '<h1 class="page-title">', '</h1>' );
+			//	the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
+			<?php
+			/* Start the Loop */
+
+                while ( have_posts() ) :
+                    the_post();
+                    $mois = get_the_date('m');
+                    $column = ($mois % 3) + 1;
+                    $row = get_the_date('j');
+                    //var_dump($column, $row);
+                    echo'<div class="elm_Evenement" style="grid-area:'.$row.'/'.$column.'">';
+                        echo '<a href='.get_permalink().'>' . get_the_title( get_post() ) . ' :'; echo get_the_date(' j m y').  '</a>';   
+                    echo '</div>';
+                   endwhile;
+
+		endif;
+		?>
+        </div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
+////////////////////////////////////////////////////////
+
+
 
 ///////////////////////////////////////// Événements /////////////////////////////////////////////////////
+/*
+get_header();
 echo '<h1 class="titreSections"> '.category_description( get_category_by_slug( 'evenements' ) ). '</h1>'   ;
          
 echo '<div class="evenements">';
  
-    while ( $query3->have_posts() ) {
-        $query3->the_post();
-
+   // while ( $query3->have_posts() ) {
+       // $query3->the_post();
+       while ( have_posts() ) :
+        the_post();
         $mois = get_the_date('m');
         $column = ($mois % 3) + 1;
         $row = get_the_date('j');
         //var_dump($column, $row);
         echo'<div class="elm_Evenement" style="grid-area:'.$row.'/'.$column.'">';
-            echo '<a href='.get_permalink().'>' . get_the_title( $query3->post->ID ) . ' :'; echo get_the_date(' j m y').  '</a>';   
+            echo '<a href='.get_permalink().'>' . get_the_title( get_post() ) . ' :'; echo get_the_date(' j m y').  '</a>';   
         echo '</div>';
-    }
+       endwhile;
     // Restore original Post Data
     wp_reset_postdata();
 
@@ -44,3 +79,4 @@ echo '</div>';
 
 get_footer();
 ?>
+*/
--- functions.php
+++ functions.php
@@ -1,6 +1,32 @@
 <?php
+
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
- 
+     $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version'));
+    wp_enqueue_script( 'main_js',get_stylesheet_directory_uri() . '/js/main.js', NULL, 1.0,true);
 }
+
+/* Permet d'adapter la requête principale avant qu'elle ne s'exécute */ 
+function extraire_evenement( $query ) {
+
+   if (!is_home() && $query->is_category('evenement'))
+   {
+      $query->set( 'posts_per_page', -1 );
+      $query->set( 'orderby', 'date' );
+      $query->set( 'order', 'asc' );
+   }
+}
+add_action( 'pre_get_posts', 'extraire_evenement' );
+
+
+
+
+
+
+
