df5066f3430eed9cf75d5600f661ec37fdf21c7c
Tsingakis MP <marie-pier.1997@hotmail.com>
2020-03-11 17:10:08 -0400

    Ex10 optimisation code

--- category-nouvelle.php
+++ category-nouvelle.php
@@ -13,17 +13,6 @@
  */
 
 
-// The Query
-$args = array(
-    "category_name" => "nouvelle",
-    //"posts_per_page" => 3, //afficher les 3 derniere nouvelles "posts_per_page" => "3"
-   // "orderby" =>"date",
-   // "order" => "ASC"
-);
-
-$query1 = new WP_Query( $args );
-
-
 
 get_header();
 ?>
@@ -48,10 +37,10 @@ get_header();
         echo '<h1 class="titreSections"> '.category_description( get_category_by_slug( 'nouvelle' ) ). '<h1>'   ;
         echo '<div class="conference">';
             // The Loop
-            while ( $query1->have_posts() ) {
+            while (have_posts() ) {
                 
-                $query1->the_post();
-                $heure= substr(get_post_field("post_name"),-2);
+                the_post();
+               // $heure= substr(get_post_field("post_name"),-2);
                 
                 echo '<div class="conferenceItems">';
     
@@ -61,7 +50,7 @@ get_header();
                         echo '<p>' . get_the_title() .'</p>';
                         echo '<p>' . SUBSTR(get_the_excerpt(),0,200) . '</p>';
                         echo  '<input type="button"  class="btnNouvelle" id="'.get_the_ID().'"value="Lire la suite..."></input>';
-                        echo  '<div data-id="'.get_the_ID().'" > </div>';
+                        echo  '<div data-id="'.get_the_ID().'" > </div>';//Pour détecter l'état visible
                     echo '</div>';
                 echo '</div>';
             }
